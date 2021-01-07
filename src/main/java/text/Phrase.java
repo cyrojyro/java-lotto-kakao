c552725b167d84f3d0678b456d87efd4a6d7b8c4
jack.won <jack.won@kakaocorp.com>
2021-01-07 16:33:42 +0900

    feat(Phrase): 프로그램 사용 문구 저장소 추가
    * public static final로 정의

---
+++ src/main/java/text/Phrase.java
@@ -0,0 +1,9 @@
+package text;
+
+public class Phrase {
+    public static final String BUY_AMOUNT_PHRASE = "구입금액을 입력해 주세요.";
+    public static final String LOTTOS_BOUGHT_PHRASE = "개를 구매했습니다.";
+    public static final String LAST_WEEK_BALLS_PHRASE = "지난 주 당첨 번호를 입력해 주세.";
+    public static final String BONUS_BALL_INPUT_PHRASE= "보너스 볼을 입력해 주세요";
+    public static final String WINNING_STATISTICS_PHRASE = "당첨 통계\n---------";
+}
